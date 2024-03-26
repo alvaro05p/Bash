753bca4a0754d237a292906683e99ca98978333e
alvaro05p <alvaro05pomg@gmail.com>
2024-03-26 13:14:24 +0100

    apuntes

---
+++ EjercicioAlvaro.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#Este en un ejericicio que he hecho para estudiar:
+
+	echo "Menu: "
+	echo "1.Listar los archivos y directorios de donde estas"
+	echo "2.Cuantos archivos y directorios hay aqui"
+	echo "3.Calcular el tamaño total de bytes de todos los archivos"
+	echo "4.Muestra los 5 archivos mas grandes"
+	echo "5.Comprimir el directorio en zip"
+	echo
+	read -p "Opcion" opcion
+
+case $opcion in
+
+	1)
+		ls
+		;;
+	2)
+		ls | wc -f
+		;;
+	3)
+                ;;
+        4)
+                ;;
+	5)
+		;;
+
+esac
